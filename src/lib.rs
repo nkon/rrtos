2904d1556f610b47bb6c228c38754efab3416f0d
Kondou,Nobuhiro <kondou.nobuhiro@gmail.com>
2024-11-29 11:11:49 -0600

    Impl mutex(not tested)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![no_std]
 pub mod linked_list;
+pub mod mutex;
 pub mod process;
 pub mod scheduler;
---
+++ src/mutex.rs
@@ -0,0 +1,56 @@
+use core::cell::UnsafeCell;
+use core::ops::{Deref, DerefMut};
+use core::sync::atomic::{AtomicBool, Ordering};
+
+pub struct Mutex<T> {
+    locked: AtomicBool,
+    data: UnsafeCell<T>,
+}
+
+pub struct MutexGuard<'a, T> {
+    lock: &'a Mutex<T>,
+}
+
+impl<'a, T> MutexGuard<'a, T> {
+    fn new(lock: &'a Mutex<T>) -> Self {
+        MutexGuard { lock }
+    }
+}
+
+impl<T> Deref for MutexGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe { &mut *self.lock.data.get() }
+    }
+}
+
+impl<T> DerefMut for MutexGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.lock.data.get() }
+    }
+}
+
+impl<T> Drop for MutexGuard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.unlock();
+    }
+}
+
+impl<T> Mutex<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+            data: UnsafeCell::new(value),
+        }
+    }
+    pub fn lock(&self) -> MutexGuard<'_, T> {
+        // Cortex-M0+ does not support AtmicBool::compare_exchange()
+        // Do nothing now
+        MutexGuard::new(self)
+    }
+    fn unlock(&self) {
+        self.locked.store(false, Ordering::Release);
+    }
+}
+
+unsafe impl<T> Sync for Mutex<T> {}
